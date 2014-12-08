b108ef64ba5cb6f13db1fee7ff571e6fc67395eb
AustinHay <hayaustin@gmail.com>
2014-12-07 21:49:39 -0800

    Major revamps to GUI and backend data scrape.

--- app.rb
+++ app.rb
@@ -3,34 +3,70 @@ require 'sinatra'
 require 'yahoo_finance'
 require 'json'
 require 'sass'
+require 'csv'
 
 get '/' do
 
-@stocks = ["AAPL",
-			"GOOG",
-			"MSFT",
-			"AMZN",
-			"NFLX",
-			"TSLA",
-			"SCTY",
-			"VZ",
-			"OPWR",
-			"PANW",
-			"WAG",
-			"CYBR",
-			"WFM"]
+@stocks = []
+i = 0
+csv_raw_data = File.read('public/data/stocks.csv')
+	csv_new_data = CSV.parse(csv_raw_data, :headers => true)
+	csv_new_data.each do |row|
+	@stocks[i] = row
+	i += 1
+end
 
-@metrics = [:symbol,
+
+@metrics = [:name,
+			:symbol,
+			:change,
 			:ask,
 			:bid,
-			:change,
+			:moving_average_50_day,
+     		:moving_average_200_day,
+			:after_hours_change_real_time,
 			:earnings_per_share,
+			:eps_estimate_next_quarter,
 			:dividend_yield,
+			:ex_dividend_date,
 			:pe_ratio, 
 			:high_52_weeks,
-			:low_52_weeks]
+			:low_52_weeks,
+			:annualized_gain,
+			:market_capitalization]
 
-p data = YahooFinance.quotes(stocks,metrics)
+@metric_names = [ 
+			"Symbol", 
+			"Change", 
+			"Ask", 
+			"Bid", 
+			"50 Day Moving Avg.", 
+     		"200 Day Moving Avg.", 
+			"After Hours +/-", 
+			"EPS", 
+			"FQ EPS Projection", 
+			"Div Yield", 
+			"EX Div Date", 
+			"PE",  
+			"52 Week High", 
+			"52 Week Low", 
+			"Annualized Gain", 
+			"Market Cap"]
+
+@num = []
+@num = @stocks.count
+
+@data = []
+p @data = YahooFinance.quotes(@stocks,@metrics)
+p @data[0].name
+
+erb :index,  :locals => {:num => @num,
+							:stocks => @stocks, 
+							:metrics => @metrics,
+							:metric_names => @metric_names,
+							:data => @data,
+							}
+
 
 # YahooFinance.quotes do |x|
 # 	p x.key
@@ -38,4 +74,4 @@ p data = YahooFinance.quotes(stocks,metrics)
 
 # data.each do |x|
 # 	p 
-
+end
