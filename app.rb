debf23a11f3cd3cc2cd68b9b0e52f9d6e9555504
AustinHay <hayaustin@gmail.com>
2014-12-17 07:20:11 -0800

    Updated views and logic.

--- app.rb
+++ app.rb
@@ -8,6 +8,11 @@ require 'csv'
 get '/' do
 
 @stocks = []
+
+@stocks = File.open('/' + params['uploaded_stocks'][:filename], "w") do |f|
+    f.write(params['uploaded_stocks'][:tempfile].read)
+  		end
+
 i = 0
 csv_raw_data = File.read('public/data/stocks.csv')
 	csv_new_data = CSV.parse(csv_raw_data, :headers => true)
@@ -16,12 +21,11 @@ csv_raw_data = File.read('public/data/stocks.csv')
 	i += 1
 end
 
-
 @metrics = [:name,
 			:symbol,
 			:change,
-			:ask,
-			:bid,
+			:last_trade_price,
+			:close,
 			:moving_average_50_day,
      		:moving_average_200_day,
 			:after_hours_change_real_time,
@@ -32,25 +36,23 @@ end
 			:pe_ratio, 
 			:high_52_weeks,
 			:low_52_weeks,
-			:annualized_gain,
 			:market_capitalization]
 
 @metric_names = [ 
-			"Symbol", 
+			"Symbol",
+			"Last Trade Price",  
 			"Change", 
-			"Ask", 
-			"Bid", 
+			"Close", 
+			"EX Div Date",
+			"Div Yield",   
+			"52 Week High", 
+			"52 Week Low",
 			"50 Day Moving Avg.", 
      		"200 Day Moving Avg.", 
 			"After Hours +/-", 
 			"EPS", 
-			"FQ EPS Projection", 
-			"Div Yield", 
-			"EX Div Date", 
-			"PE",  
-			"52 Week High", 
-			"52 Week Low", 
-			"Annualized Gain", 
+			"FQ EPS Projection",  
+			"PE", 
 			"Market Cap"]
 
 @num = []
@@ -60,18 +62,12 @@ end
 p @data = YahooFinance.quotes(@stocks,@metrics)
 p @data[0].name
 
-erb :index,  :locals => {:num => @num,
-							:stocks => @stocks, 
+erb :index,  :locals => {   :stocks => @stocks, 
 							:metrics => @metrics,
 							:metric_names => @metric_names,
 							:data => @data,
 							}
 
 
-# YahooFinance.quotes do |x|
-# 	p x.key
-# end
-
-# data.each do |x|
-# 	p 
+:uploaded_stocks
 end
